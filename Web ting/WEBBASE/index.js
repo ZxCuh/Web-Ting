74ce4cb9948fc57984e88da1e4dd72303e149930
Zx <119522066+ZxCuh@users.noreply.github.com>
2025-12-17 08:51:35 +0000

    added past files

---
+++ Web ting/WEBBASE/index.js
@@ -0,0 +1,19 @@
+function displayBio(){
+    document.getElementById("biology").style.display = "inline";
+}
+function displayMath(){
+    document.getElementById("math").style.display = "inline";
+}
+function  displayCS(){
+    document.getElementById("cs").style.display = "inline";
+}
+function displayEng(){
+    document.getElementById("en").style.display = "inline";
+    document.getElementById("en").exitFullscreen();
+}
+function displayChem(){
+    document.getElementById("Chem").style.display = "inline";
+}
+function displayPhys() {
+    document.getElementById("phys").style.display = "inline";
+}
